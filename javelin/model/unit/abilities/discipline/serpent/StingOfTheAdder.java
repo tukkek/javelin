b4bc0b9652ec745e14f18879c65571d501269094
Alex Henry <javelinrl@gmail.com>
2017-09-12 20:58:31 -0300

    StingOfTheAdder

---
+++ javelin/model/unit/abilities/discipline/serpent/StingOfTheAdder.java
@@ -0,0 +1,76 @@
+package javelin.model.unit.abilities.discipline.serpent;
+
+import javelin.Javelin;
+import javelin.controller.action.ActionCost;
+import javelin.controller.action.ai.attack.DamageChance;
+import javelin.model.state.BattleState;
+import javelin.model.unit.abilities.discipline.Strike;
+import javelin.model.unit.attack.Attack;
+import javelin.model.unit.attack.Combatant;
+import javelin.model.unit.condition.Condition;
+import javelin.model.unit.condition.abilitydamage.WisdomDamage;
+import tyrant.mikera.engine.RPG;
+
+public class StingOfTheAdder extends Strike {
+	static final int DAMAGE = RPG.average(5, 6);
+	static final int WISDOMDAMAGE = RPG.average(1, 4);
+	static final int SECONDARYDAMAGE = RPG.average(1, 6);
+	static final int SECONDARYWISDOMDAMAGE = 2;
+
+	class AdderSting extends Condition {
+		public AdderSting(float expireatp, Combatant c) {
+			super(expireatp, c, Effect.NEGATIVE, "adder sting", null);
+		}
+
+		@Override
+		public void start(Combatant c) {
+			// wait to expire
+		}
+
+		@Override
+		public void end(Combatant c) {
+			c.hp -= SECONDARYDAMAGE;
+			if (c.hp < 1) {
+				c.hp = 1;
+			}
+			c.addcondition(new WisdomDamage(SECONDARYWISDOMDAMAGE, c));
+		}
+	}
+
+	public StingOfTheAdder() {
+		super("Sting of the adder", 4);
+		ap = ActionCost.STANDARD;
+	}
+
+	@Override
+	public void preattacks(Combatant current, Combatant target, Attack a,
+			BattleState s) {
+		// nothing
+	}
+
+	@Override
+	public void postattacks(Combatant current, Combatant target, Attack a,
+			BattleState s) {
+		// nothing
+	}
+
+	@Override
+	public void prehit(Combatant current, Combatant target, Attack a,
+			DamageChance dc, BattleState s) {
+		if (!Javelin.DEBUG) {
+			dc.damage += DAMAGE;
+		}
+		final boolean save = save(target.source.fortitude(), 14, current);
+		final int wisdomdamage = save ? WISDOMDAMAGE / 2 : WISDOMDAMAGE;
+		target.addcondition(new WisdomDamage(wisdomdamage, target));
+		if (!save) {
+			target.addcondition(new AdderSting(current.ap + 1, target));
+		}
+	}
+
+	@Override
+	public void posthit(Combatant current, Combatant target, Attack a,
+			DamageChance dc, BattleState s) {
+		// nothing
+	}
+}

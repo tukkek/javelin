45e8a631fb71a46404571be33be18edfe4043976
Alex Henry <javelinrl@gmail.com>
2017-09-12 16:29:39 -0300

    Cleaning debug file

--- javelin/Debug.java
+++ javelin/Debug.java
@@ -6,8 +6,6 @@ import javelin.controller.fight.Fight;
 import javelin.controller.scenario.Scenario;
 import javelin.model.item.Item;
 import javelin.model.unit.Squad;
-import javelin.model.unit.abilities.discipline.serpent.IronFang;
-import javelin.model.unit.abilities.discipline.serpent.SteelSerpent;
 import javelin.model.unit.attack.Combatant;
 import javelin.model.world.location.town.Town;
 import javelin.view.screen.WorldScreen;
@@ -76,8 +74,6 @@ public class Debug {
 	}
 
 	public static String onbattlehelp() {
-		Helpers.freezeopponents();
-		Helpers.healopponenets();
 		return "";
 	}
 
@@ -87,9 +83,7 @@ public class Debug {
 
 	/** Called only once when a {@link Scenario} is initialized. */
 	public static void oncampaignstart() {
-		for (Combatant c : Squad.active.members) {
-			c.addmaneuver(SteelSerpent.INSTANCE, new IronFang());
-		}
+
 	}
 
 	/**

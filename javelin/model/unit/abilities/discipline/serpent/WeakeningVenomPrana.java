71bd70bdfd232f85a689707b4aa681fb9286a75c
Alex Henry <javelinrl@gmail.com>
2017-09-12 18:11:18 -0300

    WeakeningVenomPrana

---
+++ javelin/model/unit/abilities/discipline/serpent/WeakeningVenomPrana.java
@@ -0,0 +1,43 @@
+package javelin.model.unit.abilities.discipline.serpent;
+
+import javelin.controller.action.ai.attack.DamageChance;
+import javelin.model.state.BattleState;
+import javelin.model.unit.abilities.discipline.Strike;
+import javelin.model.unit.attack.Attack;
+import javelin.model.unit.attack.Combatant;
+import javelin.model.unit.condition.abilitydamage.StrengthDamage;
+import tyrant.mikera.engine.RPG;
+
+public class WeakeningVenomPrana extends Strike {
+	static final int DAMAGE = RPG.average(1, 4);
+
+	public WeakeningVenomPrana() {
+		super("Weakening venom prana", 2);
+	}
+
+	@Override
+	public void preattacks(Combatant current, Combatant target, Attack a,
+			BattleState s) {
+		// nothing
+	}
+
+	@Override
+	public void postattacks(Combatant current, Combatant target, Attack a,
+			BattleState s) {
+		// nothing
+	}
+
+	@Override
+	public void prehit(Combatant current, Combatant target, Attack a,
+			DamageChance dc, BattleState s) {
+		final int damage = save(target.source.fortitude(), 12, current)
+				? DAMAGE / 2 : DAMAGE;
+		target.addcondition(new StrengthDamage(damage, target));
+	}
+
+	@Override
+	public void posthit(Combatant current, Combatant target, Attack a,
+			DamageChance dc, BattleState s) {
+		// nothing
+	}
+}

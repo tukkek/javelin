f2557913465cc6cb1943fdca84cf81e45025573e
Alex Henry <tukkek@gmail.com>
2018-08-18 11:38:52 -0300

    New dungeon battle map: Constructed

--- javelin/controller/terrain/Underground.java
+++ javelin/controller/terrain/Underground.java
@@ -1,7 +1,10 @@
 package javelin.controller.terrain;
 
+import java.util.List;
+
 import javelin.controller.map.Map;
 import javelin.controller.map.Maps;
+import javelin.controller.map.terrain.underground.Constructed;
 import javelin.model.world.location.dungeon.Dungeon;
 import javelin.old.underground.BigCave;
 import javelin.old.underground.Caves;
@@ -16,22 +19,23 @@ import javelin.view.Images;
  *
  * @author alex
  */
-public class Underground extends Terrain {
+public class Underground extends Terrain{
 	/** Constructor. */
-	public Underground() {
-		name = "underground";
-		difficulty = 0;
-		difficultycap = Integer.MAX_VALUE;
+	public Underground(){
+		name="underground";
+		difficulty=0;
+		difficultycap=Integer.MAX_VALUE;
 	}
 
 	@Override
-	public Maps getmaps() {
-		Maps maps = new Maps();
-		for (Map m : new Map[] { new Caves(), new BigCave(), new Maze(),
-				new Pit(), new Floor(), new Complex() }) {
-			if (Dungeon.active != null) {
-				m.floor = Images.getImage(Dungeon.active.floor);
-				m.wall = Images.getImage(Dungeon.active.wall);
+	public Maps getmaps(){
+		Maps maps=new Maps();
+		for(Map m:List.of(new Caves(),new BigCave(),new Maze(),new Pit(),
+				new Floor(),new Complex(),new Constructed())){
+			Dungeon d=Dungeon.active;
+			if(d!=null){
+				m.floor=Images.getImage(d.floor);
+				m.wall=Images.getImage(d.wall);
 			}
 			maps.add(m);
 		}

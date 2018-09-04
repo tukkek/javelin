686c9cc9ac224f98adf8a93a6c9014f50a8a9260
Alex Henry <tukkek@gmail.com>
2018-09-04 17:33:16 -0300

    Prevent ConcurrentModificationException on Overlay#clear #228

--- javelin/view/mappanel/Overlay.java
+++ javelin/view/mappanel/Overlay.java
@@ -6,37 +6,35 @@ import java.util.ArrayList;
 import javelin.controller.Point;
 import javelin.view.screen.BattleScreen;
 
-public abstract class Overlay {
-	public ArrayList<Point> affected = new ArrayList<Point>();
+public abstract class Overlay{
+	public ArrayList<Point> affected=new ArrayList<>();
 
 	abstract public void overlay(Tile t);
 
-	public void clear() {
-		MapPanel.overlay = null;
-		final Tile[][] tiles = BattleScreen.active.mappanel.tiles;
-		for (Point p : affected) {
-			try {
+	public void clear(){
+		MapPanel.overlay=null;
+		final Tile[][] tiles=BattleScreen.active.mappanel.tiles;
+		for(Point p:new ArrayList<>(affected))
+			try{
 				tiles[p.x][p.y].repaint();
-			} catch (IndexOutOfBoundsException e) {
+			}catch(IndexOutOfBoundsException e){
 				continue;// TODO
 			}
-		}
 		BattleScreen.active.mappanel.refresh();
 	}
 
 	/**
 	 * Draws image on given tile and adds it to #affected.
 	 */
-	protected void draw(Tile t, Image i) {
-		Point p = t.getposition();
-		BattleScreen.active.mappanel.getdrawgraphics().drawImage(i, p.x, p.y,
-				MapPanel.tilesize, MapPanel.tilesize, null);
-		affected.add(new Point(t.x, t.y));
+	protected void draw(Tile t,Image i){
+		Point p=t.getposition();
+		BattleScreen.active.mappanel.getdrawgraphics().drawImage(i,p.x,p.y,
+				MapPanel.tilesize,MapPanel.tilesize,null);
+		affected.add(new Point(t.x,t.y));
 	}
 
-	public void refresh(MapPanel mappanel) {
-		for (Point p : affected) {
+	public void refresh(MapPanel mappanel){
+		for(Point p:affected)
 			mappanel.tiles[p.x][p.y].repaint();
-		}
 	}
 }

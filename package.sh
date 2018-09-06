daf4f20144bc21375ccbb247492e6fb08b25d65f
Alex Henry <tukkek@gmail.com>
2018-09-06 12:02:41 -0300

    package.sh: version prompt

--- package.sh
+++ package.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 # simple assembly system to package a .zip file for distribution
+version=`git log --oneline -1 --decorate`
+read -e -i "$version" -p "Edit version name: " version
 ant
 mkdir package
 mkdir package/javelin
 cp javelin.jar package/javelin
 cp -r doc package/javelin
 rm package/javelin/doc/.*~
-echo "`git log --oneline -1 --decorate`">package/javelin/doc/VERSION.txt
+echo $version>package/javelin/doc/VERSION.txt
 cp -r avatars/ package/javelin
 cp -r maps/ package/javelin
 cp monsters.xml package/javelin

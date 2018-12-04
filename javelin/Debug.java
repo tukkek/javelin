2c951754716267537e036610d28abda50a44757a
Alex Henry <javelinrl@gmail.com>
2018-12-04 10:24:18 -0200

    Reverting changes in Debug

--- javelin/Debug.java
+++ javelin/Debug.java
@@ -2,7 +2,6 @@ package javelin;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.IntStream;
 
 import javelin.controller.action.Help;
 import javelin.controller.challenge.ChallengeCalculator;
@@ -14,10 +13,8 @@ import javelin.controller.map.Map;
 import javelin.controller.scenario.Scenario;
 import javelin.model.Realm;
 import javelin.model.item.Item;
-import javelin.model.item.Potion;
 import javelin.model.unit.Combatant;
 import javelin.model.unit.Squad;
-import javelin.model.unit.abilities.spell.conjuration.healing.wounds.CureLightWounds;
 import javelin.model.world.Actor;
 import javelin.model.world.Incursion;
 import javelin.model.world.World;
@@ -186,12 +183,6 @@ public class Debug{
 	 */
 	public static String onworldhelp(){
 		String s="";
-		IntStream.range(0,5).forEach(i->{
-			Combatant c=new Combatant(Javelin.getmonster("kobold"),false);
-			c.setmercenary(true);
-			Squad.active.add(c);
-			Squad.active.equipment.get(c).add(new Potion(new CureLightWounds()));
-		});
 		return s;
 	}
 }

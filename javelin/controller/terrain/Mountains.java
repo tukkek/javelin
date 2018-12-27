bfb2d771954136e401957d4197906bea435c7d0e
Alex Henry <tukkek@gmail.com>
2018-12-27 11:59:30 -0200

    MountainsOfMadness winter/storm mountain map

--- javelin/controller/terrain/Mountains.java
+++ javelin/controller/terrain/Mountains.java
@@ -3,14 +3,17 @@ package javelin.controller.terrain;
 import java.util.Set;
 
 import javelin.controller.Point;
+import javelin.controller.Weather;
 import javelin.controller.map.Maps;
 import javelin.controller.map.terrain.hill.Rugged;
 import javelin.controller.map.terrain.mountain.Forbidding;
 import javelin.controller.map.terrain.mountain.Meadow;
+import javelin.controller.map.terrain.mountain.MountainsOfMadness;
 import javelin.controller.terrain.hazard.Break;
 import javelin.controller.terrain.hazard.Cold;
 import javelin.controller.terrain.hazard.Hazard;
 import javelin.controller.terrain.hazard.Rockslide;
+import javelin.model.world.Season;
 import javelin.model.world.World;
 
 /**
@@ -37,6 +40,8 @@ public class Mountains extends Terrain{
 		m.add(new Meadow());
 		m.add(new Rugged());
 		m.add(new Forbidding());
+		if(Weather.current==Weather.STORM||Season.current==Season.WINTER)
+			m.add(new MountainsOfMadness());
 		return m;
 	}
 

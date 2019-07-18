a78956dd3fe2a75a852233a494680c6b0180df72
Alex Henry <tukkek@gmail.com>
2019-07-18 00:17:15 -0300

    DarkShrine map (and others which were missing)

--- javelin/model/world/location/haunt/DarkShrine.java
+++ javelin/model/world/location/haunt/DarkShrine.java
@@ -1,11 +1,18 @@
 package javelin.model.world.location.haunt;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
+import javelin.controller.Point;
+import javelin.controller.map.location.LocationMap;
 import javelin.controller.terrain.Terrain;
+import javelin.model.state.Square;
 import javelin.model.unit.Monster;
 import javelin.model.unit.Monster.MonsterType;
+import javelin.old.RPG;
+import javelin.view.Images;
 
 /**
  * Haunt for evil {@link MonsterType#OUTSIDER}s, demons, devils, etc.
@@ -13,6 +20,7 @@ import javelin.model.unit.Monster.MonsterType;
  * @author alex
  */
 public class DarkShrine extends Haunt{
+
 	static final List<String> SUBTYPES=List.of("demon (tanar'ri)","devil","evil");
 	static final List<Monster> POOL=Monster.MONSTERS
 			.stream().filter(m->m.type.equals(MonsterType.OUTSIDER)
@@ -20,8 +28,54 @@ public class DarkShrine extends Haunt{
 			.collect(Collectors.toList());
 	static final List<Terrain> TERRAINS=List.of(Terrain.FOREST,Terrain.MARSH);
 
+	/** Dark shrine map. */
+	public static class ShrineMap extends LocationMap{
+		Set<Point> area=new HashSet<>();
+		Set<Point> seeds=new HashSet<>();
+		Set<Point> blocked=new HashSet<>();
+
+		/** Constructor. */
+		public ShrineMap(){
+			super("Dark shrine");
+			wall=Images.get("terrainorcwall");
+			floor=Images.get("terraintraininghall");
+		}
+
+		@Override
+		protected Square processtile(Square s,int x,int y,char c){
+			if(c=='1')
+				spawnblue.add(new Point(x,y));
+			else if(c=='2')
+				spawnred.add(new Point(x,y));
+			else if(c==' ')
+				area.add(new Point(x,y));
+			else
+				return super.processtile(s,x,y,c);
+			return s;
+		}
+
+		@Override
+		public void generate(){
+			super.generate();
+			var nseeds=RPG.rolldice(10,4);
+			while(seeds.size()<nseeds)
+				seeds.add(RPG.pick(area));
+			var target=area.size()*1.0/RPG.r(2,3);
+			while(blocked.size()<target){
+				var seed=RPG.pick(seeds);
+				var move=new Point(seed);
+				move.displace();
+				if(!validate(move.x,move.y)||!area.contains(move)) continue;
+				blocked.add(move);
+				map[move.x][move.y].blocked=true;
+				seed.x=move.x;
+				seed.y=move.y;
+			}
+		}
+	}
+
 	/** Constructor. */
 	public DarkShrine(){
-		super("Dark shrine",null,POOL,TERRAINS);
+		super("Dark shrine",ShrineMap.class,POOL,TERRAINS);
 	}
 }

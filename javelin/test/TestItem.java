6240bc62c9783ced7e3dad6146bfb817ee0c5b53
Alex Henry <tukkek@gmail.com>
2020-10-30 04:02:53 -0300

    Adding `print` argument to TestItem#testrange()

--- javelin/test/TestItem.java
+++ javelin/test/TestItem.java
@@ -20,11 +20,12 @@ public class TestItem{
 	 * @param items Given these items, checks their {@link Item#price} to see on
 	 *          how many {@link Tier}s they belong in. Generally speaking, good
 	 *          items types will be accessible in as many tiers as possible.
+	 * @param If <code>true</code> will print information to {@link System#out}.
 	 * @return The median number of items per tier. This can be used to compare
 	 *         different approaches and parameters to systems - a higher number is
 	 *         preferrable, generally speaking.
 	 */
-	public static int testrange(List<Item> items){
+	public static int testrange(List<Item> items,boolean print){
 		var hits=new Integer[]{0,0,0,0};
 		for(var i:items)
 			for(int t=0;t<=4;t++){
@@ -38,9 +39,13 @@ public class TestItem{
 					break;
 				}
 			}
-		System.out.println(Arrays.asList(hits));
 		var result=new ArrayList<>(Arrays.asList(hits));
 		result.sort(null);
-		return (result.get(1)+result.get(2))/2;
+		int median=(result.get(1)+result.get(2))/2;
+		if(print){
+			System.out.println(Arrays.asList(hits));
+			System.out.println("Median: "+median);
+		}
+		return median;
 	}
 }

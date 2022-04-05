1659bdf9f233d3113ff0aeee7fd05c3e60ccd832
Alex Henry <tukkek@gmail.com>
2022-04-05 19:59:30 -0300

    EncounterIndex#expand(): culling EL range

--- javelin/controller/db/EncounterIndex.java
+++ javelin/controller/db/EncounterIndex.java
@@ -31,141 +31,134 @@ import javelin.old.RPG;
  * @author alex
  */
 public class EncounterIndex extends TreeMap<Integer,List<Encounter>>{
-	/** Constructor. */
-	public EncounterIndex(){
-		super();
-	}
+  /** Constructor. */
+  public EncounterIndex(){}
 
-	/** Constructor from {@link Monster}s, with NPCs and mixed encounters. */
-	public EncounterIndex(List<Monster> pool){
-		for(var m:pool){
-			for(var i=1;i<=Encounter.BIG;i++)
-				put(new Encounter(m,i));
-			var from=Math.round(m.cr)+1;
-			var to=Math.max(20,from+10);
-			IntStream.rangeClosed(from,to)
-					.mapToObj(level->NpcGenerator.generate(m,level))
-					.filter(npc->npc!=null).forEach(npc->put(new Encounter(npc)));
-		}
-		var all=values().stream().flatMap(e->e.stream())
-				.collect(Collectors.toList());
-		for(var i=0;i<all.size();i++)
-			for(var j=i+1;j<all.size();j++){
-				var a=all.get(i).group;
-				var b=all.get(j).group;
-				var size=a.size()+b.size();
-				if(size>Encounter.BIG) continue;
-				var mixed=new Combatants(size);
-				mixed.addAll(a);
-				mixed.addAll(b);
-				try{
-					ChallengeCalculator.calculateelsafe(mixed);
-				}catch(UnbalancedTeams e){
-					continue;
-				}
-				if(new CountingSet(mixed).getcount().size()>1)
-					put(new Encounter(mixed));
-			}
-	}
+  /** Constructor from {@link Monster}s, with NPCs and mixed encounters. */
+  public EncounterIndex(List<Monster> pool){
+    for(var m:pool){
+      for(var i=1;i<=Encounter.BIG;i++) put(new Encounter(m,i));
+      var from=Math.round(m.cr)+1;
+      var to=Math.max(20,from+10);
+      IntStream.rangeClosed(from,to)
+          .mapToObj(level->NpcGenerator.generate(m,level))
+          .filter(npc->npc!=null).forEach(npc->put(new Encounter(npc)));
+    }
+    var all=values().stream().flatMap(List::stream)
+        .collect(Collectors.toList());
+    for(var i=0;i<all.size();i++) for(var j=i+1;j<all.size();j++){
+      var a=all.get(i).group;
+      var b=all.get(j).group;
+      var size=a.size()+b.size();
+      if(size>Encounter.BIG) continue;
+      var mixed=new Combatants(size);
+      mixed.addAll(a);
+      mixed.addAll(b);
+      try{
+        ChallengeCalculator.calculateelsafe(mixed);
+      }catch(UnbalancedTeams e){
+        continue;
+      }
+      if(new CountingSet(mixed).getcount().size()>1) put(new Encounter(mixed));
+    }
+  }
 
-	/** @param e Register this with the given Encounter Level. */
-	public void put(int el,Encounter e){
-		var tier=get(el);
-		if(tier==null){
-			tier=new ArrayList<>();
-			put(el,tier);
-		}
-		tier.add(e);
-	}
+  /** @param e Register this with the given Encounter Level. */
+  public void put(int el,Encounter e){
+    var tier=get(el);
+    if(tier==null){
+      tier=new ArrayList<>();
+      put(el,tier);
+    }
+    tier.add(e);
+  }
 
-	/** @return Total number of {@link Encounter}s. */
-	public int count(){
-		int count=0;
-		for(var encounters:values())
-			count+=encounters.size();
-		return count;
-	}
+  /** @return Total number of {@link Encounter}s. */
+  public int count(){
+    var count=0;
+    for(var encounters:values()) count+=encounters.size();
+    return count;
+  }
 
-	/** Calls {@link #put(int, Encounter)} with {@link Encounter#el}. */
-	public void put(Encounter e){
-		put(e.el,e);
-	}
+  /** Calls {@link #put(int, Encounter)} with {@link Encounter#el}. */
+  public void put(Encounter e){
+    put(e.el,e);
+  }
 
-	/** Adds all of the entires of the given Index to this one. */
-	public void merge(EncounterIndex i){
-		for(var el:i.keySet())
-			merge(el,new ArrayList<>(i.get(el)),(a,b)->{
-				a.addAll(b);
-				return a;
-			});
-	}
+  /** Adds all of the entires of the given Index to this one. */
+  public void merge(EncounterIndex i){
+    for(var el:i.keySet()) merge(el,new ArrayList<>(i.get(el)),(a,b)->{
+      a.addAll(b);
+      return a;
+    });
+  }
 
-	/**
-	 * @return A new index with only {@link Encounter}s in the given Encounter
-	 *         Level range (inclusive).
-	 */
-	public EncounterIndex filter(int from,int to){
-		var i=new EncounterIndex();
-		for(var el:keySet())
-			if(from<=el&&el<=to) i.put(el,get(el));
-		return i;
-	}
+  /**
+   * @return A new index with only {@link Encounter}s in the given Encounter
+   *   Level range (inclusive).
+   */
+  public EncounterIndex filter(int from,int to){
+    var i=new EncounterIndex();
+    for(var el:keySet()) if(from<=el&&el<=to) i.put(el,get(el));
+    return i;
+  }
 
-	/**
-	 * @return As {@link #filter(int, int)} but in the {@link Difficulty#VERYEASY}
-	 *         to {@link Difficulty#DIFFICULT} range.
-	 */
-	public EncounterIndex filter(int el){
-		var from=el+Difficulty.VERYEASY;
-		var to=el+Difficulty.DIFFICULT;
-		return filter(from,to);
-	}
+  /**
+   * @return As {@link #filter(int, int)} but in the {@link Difficulty#VERYEASY}
+   *   to {@link Difficulty#DIFFICULT} range.
+   */
+  public EncounterIndex filter(int el){
+    var from=el+Difficulty.VERYEASY;
+    var to=el+Difficulty.DIFFICULT;
+    return filter(from,to);
+  }
 
-	/** @return All {@link Encounter}s. */
-	public List<Encounter> getall(){
-		return values().stream().flatMap(es->es.stream())
-				.collect(Collectors.toList());
-	}
+  /** @return All {@link Encounter}s. */
+  public List<Encounter> getall(){
+    return values().stream().flatMap(List::stream).collect(Collectors.toList());
+  }
 
-	/**
-	 * @return A new index, truncating each Encounter Level list of
-	 *         {@link Encounter}s to a maximum, rnadomly-chosen given limit of
-	 *         entries.
-	 */
-	public EncounterIndex limit(int limit){
-		var i=new EncounterIndex();
-		for(var el:keySet()){
-			var encounters=get(el);
-			if(encounters.size()>limit) encounters=new ArrayList<>(
-					RPG.shuffle(new ArrayList<>(encounters).subList(0,limit)));
-			i.put(el,encounters);
-		}
-		return i;
-	}
+  /**
+   * @return A new index, truncating each Encounter Level list of
+   *   {@link Encounter}s to a maximum, rnadomly-chosen given limit of entries.
+   */
+  public EncounterIndex limit(int limit){
+    var i=new EncounterIndex();
+    for(var el:keySet()){
+      var encounters=get(el);
+      if(encounters.size()>limit) encounters=new ArrayList<>(
+          RPG.shuffle(new ArrayList<>(encounters).subList(0,limit)));
+      i.put(el,encounters);
+    }
+    return i;
+  }
 
-	/** {@link #merge(EncounterIndex)} many indexes into one. */
-	public static EncounterIndex merge(Collection<EncounterIndex> merge){
-		var i=new EncounterIndex();
-		for(var m:merge)
-			i.merge(m);
-		return i;
-	}
+  /** {@link #merge(EncounterIndex)} many indexes into one. */
+  public static EncounterIndex merge(Collection<EncounterIndex> merge){
+    var i=new EncounterIndex();
+    for(var m:merge) i.merge(m);
+    return i;
+  }
 
-	/**
-	 * Combines all current {@link Encounter}s to maximize encounters by Ecnounter
-	 * Level. Uses {@link AlignmentDetector}.
-	 *
-	 * Can be called multiple times but naturally can only generate higher ELs,
-	 * with {@link Encounter}s acting as the building blocks.
-	 */
-	public void expand(){
-		var encounters=getall();
-		var nencounters=encounters.size();
-		for(var i=0;i<nencounters;i++)
-			for(var j=i+1;j<nencounters;j++){
-				var e=new Combatants(encounters.get(i).group);
-				e.addAll(encounters.get(j).group);
-				if(new AlignmentDetector(e).check()) put(new Encounter(e));
-			}
-	}
+  /**
+   * Combines all current {@link Encounter}s to maximize encounters by Ecnounter
+   * Level. Uses {@link AlignmentDetector}.
+   *
+   * Can be called multiple times but naturally can only generate higher ELs,
+   * with {@link Encounter}s acting as the building blocks.
+   *
+   * @param targetel Used for culling, we don't need to expand
+   *   {@link Encounter}s outside a range that would produce a relevant EL.
+   */
+  public void expand(int targetel){
+    var encounters=keySet().stream()
+        .filter(el->targetel+Difficulty.MODERATE<el&&el<=targetel)
+        .flatMap(el->get(el).stream()).collect(Collectors.toList());
+    var nencounters=encounters.size();
+    for(var i=0;i<nencounters;i++) for(var j=i+1;j<nencounters;j++){
+      var e=new Combatants(encounters.get(i).group);
+      e.addAll(encounters.get(j).group);
+      if(new AlignmentDetector(e).check()) put(new Encounter(e));
+    }
+  }
 }
--- javelin/controller/generator/encounter/EncounterGenerator.java
+++ javelin/controller/generator/encounter/EncounterGenerator.java
@@ -25,201 +25,163 @@ import javelin.old.RPG;
  * @author alex
  */
 public class EncounterGenerator{
-	/** @see EncounterIndex#expand() */
-	static final int EXPANSIONS=2;
-	/**
-	 * So small it should be instant to {@link EncounterIndex#expand()}.
-	 *
-	 * If you divide all Index sizes in two before they get Generated here, the
-	 * lower half's ceiling is ~60 while the upper half's floor is much higer at
-	 * ~700. Basically this strategy is allowing that lower half to pre-expand
-	 * once but there is a lot of room for other indexes between 100-700 later on,
-	 * especially as more {@link Haunt}s are added - so let's be conservative here
-	 * and rise later as needed.
-	 */
-	static final int PREEXPAND=100;
-	static final boolean PRINTINFO=false;
+  /** @see EncounterIndex#expand() */
+  static final int EXPANSIONS=2;
+  /**
+   * So small it should be instant to {@link EncounterIndex#expand()}.
+   *
+   * If you divide all Index sizes in two before they get Generated here, the
+   * lower half's ceiling is ~60 while the upper half's floor is much higer at
+   * ~700. Basically this strategy is allowing that lower half to pre-expand
+   * once but there is a lot of room for other indexes between 100-700 later on,
+   * especially as more {@link Haunt}s are added - so let's be conservative here
+   * and rise later as needed.
+   */
+  static final int PREEXPAND=100;
+  static final boolean PRINTINFO=false;
 
-	static int minel=Integer.MIN_VALUE;
-	static int maxel=Integer.MAX_VALUE;
+  static int minel=Integer.MIN_VALUE;
+  static int maxel=Integer.MAX_VALUE;
 
-	/**
-	 * A helper class to abstract where the @link Monster} pool is coming from and
-	 * generate {@link Combatants} accordingly. This is helpful because
-	 * {@link Terrain} pools are vast and can generate any type of encounter level
-	 * easily while {@link EncounterIndex}es are usually more limited and thus
-	 * more slowly scan all available encounter pools.
-	 *
-	 * @author alex
-	 */
-	public static class MonsterPool{
-		List<Terrain> terrains=new ArrayList<>(0);
-		EncounterIndex index=null;
+  /**
+   * A helper class to abstract where the @link Monster} pool is coming from and
+   * generate {@link Combatants} accordingly. This is helpful because
+   * {@link Terrain} pools are vast and can generate any type of encounter level
+   * easily while {@link EncounterIndex}es are usually more limited and thus
+   * more slowly scan all available encounter pools.
+   *
+   * @author alex
+   */
+  public static class MonsterPool{
+    List<Terrain> terrains=new ArrayList<>(0);
+    EncounterIndex index=null;
 
-		/** Constructor. */
-		public MonsterPool(Terrain t){
-			terrains.add(t);
-		}
+    /** Constructor. */
+    public MonsterPool(Terrain t){
+      terrains.add(t);
+    }
 
-		/** Constructor. */
-		public MonsterPool(EncounterIndex indexp){
-			index=indexp;
-		}
+    /** Constructor. */
+    public MonsterPool(EncounterIndex indexp){
+      index=indexp;
+    }
 
-		/** Constructor. */
-		public MonsterPool(List<Monster> pool){
-			this(new EncounterIndex(pool));
-		}
+    /** Constructor. */
+    public MonsterPool(List<Monster> pool){
+      this(new EncounterIndex(pool));
+    }
 
-		/**
-		 * Calls either {@link EncounterGenerator#generate(int, EncounterIndex)} or
-		 * {@link EncounterGenerator#generate(int, List)} based on constructor data.
-		 */
-		public Combatants generate(int el) throws GaveUp{
-			return index==null?EncounterGenerator.generate(el,terrains)
-					:EncounterGenerator.generate(el,index);
-		}
-	}
+    /**
+     * Calls either {@link EncounterGenerator#generate(int, EncounterIndex)} or
+     * {@link EncounterGenerator#generate(int, List)} based on constructor data.
+     */
+    public Combatants generate(int el) throws GaveUp{
+      return index==null?EncounterGenerator.generate(el,terrains)
+          :EncounterGenerator.generate(el,index);
+    }
+  }
 
-	//	static int checklimit(int baseline,int step){
-	//		String failure=null;
-	//		for(var el=baseline;;el+=step)
-	//			for(var t:Terrain.NONWATER){
-	//				if(Javelin.DEBUG&&PRINTINFO)
-	//					failure=String.format("Failure: %s el%s",t,el);
-	//				if(generate(el,t)==null){
-	//					if(failure!=null) System.out.println(failure);
-	//					return el-step;
-	//				}
-	//			}
-	//	}
+  //	static int checklimit(int baseline,int step){
+  //		String failure=null;
+  //		for(var el=baseline;;el+=step)
+  //			for(var t:Terrain.NONWATER){
+  //				if(Javelin.DEBUG&&PRINTINFO)
+  //					failure=String.format("Failure: %s el%s",t,el);
+  //				if(generate(el,t)==null){
+  //					if(failure!=null) System.out.println(failure);
+  //					return el-step;
+  //				}
+  //			}
+  //	}
 
-	static{
-		/**
-		 * TODO ideally at some point here would use {@link Terrain#ALL} but
-		 * currently {@link Terrain#WATER} can't even reliably generate encounters
-		 * with EL less than 2, so clearly some work has got to go into adding
-		 * low-level aquatic enemies first.
-		 */
-		var encounters=EncounterIndex.merge(Terrain.NONWATER.stream()
-				.map(t->Organization.ENCOUNTERSBYTERRAIN.get(t.toString()))
-				.collect(Collectors.toList()));
-		//		for(var i=0;i<1+EXPANSIONS;i++)
-		//			encounters.expand();
-		var els=new ArrayList<>(encounters.keySet());
-		els.sort(null);
-		minel=els.get(0);
-		maxel=els.get(els.size()-1)+4;
-		//				for(var t:terrains)
-		//			encounters.add(Organization.ENCOUNTERSBYTERRAIN.get(t.toString()));)
-		//		for(var t:Terrain.NONWATER)
-		//			generate(maxel,null)
-		//		minel=checklimit(0,-1);
-		//		maxel=checklimit(0,+1);
-	}
+  static{
+    /**
+     * TODO ideally at some point here would use {@link Terrain#ALL} but
+     * currently {@link Terrain#WATER} can't even reliably generate encounters
+     * with EL less than 2, so clearly some work has got to go into adding
+     * low-level aquatic enemies first.
+     */
+    var encounters=EncounterIndex.merge(Terrain.NONWATER.stream()
+        .map(t->Organization.ENCOUNTERSBYTERRAIN.get(t.toString()))
+        .collect(Collectors.toList()));
+    var els=new ArrayList<>(encounters.keySet());
+    els.sort(null);
+    minel=els.get(0);
+    maxel=els.get(els.size()-1)+4;
+  }
 
-	public static List<Integer> COUNT=new ArrayList<>();
+  public static List<Integer> COUNT=new ArrayList<>();
 
-	/**
-	 * @param el Target encounter level - will work around this is cannot generate
-	 *          exactly what is given.
-	 * @param index Usually {@link Terrain#current()} but not necessarily - for
-	 *          example not when generation a
-	 *          {@link javelin.model.world.location.Location#garrison}, which uses
-	 *          the local terrain instead.
-	 * @return Enemy units for an encounter. <code>null</code> should not be
-	 *         returned to external calls of this class, as Encounter Levels
-	 *         should be padded to safety - the exception would be when using a
-	 *         very limited pools like only {@link Water}, or a terran that
-	 *         happens to have an empty gap in EL for some reason. In most typical
-	 *         cases it should be safe to not expect a <code>null</code> return.
-	 */
-	public static Combatants generatebyindex(int el,
-			List<EncounterIndex> encounters){
-		if(el<minel) el=minel;
-		if(el>maxel) el=maxel;
-		var index=EncounterIndex.merge(encounters);
-		var attempts=1+EXPANSIONS;
-		COUNT.add(index.count());
-		while(attempts>1&&el>index.firstKey()&&index.count()<PREEXPAND){
-			index.expand();
-			attempts-=1;
-		}
-		//		clean(index);
-		for(int i=0;i<attempts;i++){
-			if(i>0) index.expand();
-			var encounter=select(el,index);
-			if(encounter!=null) return encounter;
-			if(el<index.firstKey()) break;
-		}
-		return null;
-	}
+  /**
+   * @param el Target encounter level - will work around this is cannot generate
+   *   exactly what is given.
+   * @param index Usually {@link Terrain#current()} but not necessarily - for
+   *   example not when generation a
+   *   {@link javelin.model.world.location.Location#garrison}, which uses the
+   *   local terrain instead.
+   * @return Enemy units for an encounter. <code>null</code> should not be
+   *   returned to external calls of this class, as Encounter Levels should be
+   *   padded to safety - the exception would be when using a very limited pools
+   *   like only {@link Water}, or a terran that happens to have an empty gap in
+   *   EL for some reason. In most typical cases it should be safe to not expect
+   *   a <code>null</code> return.
+   */
+  public static Combatants generatebyindex(int el,
+      List<EncounterIndex> encounters){
+    if(el<minel) el=minel;
+    if(el>maxel) el=maxel;
+    var index=EncounterIndex.merge(encounters);
+    var attempts=1+EXPANSIONS;
+    COUNT.add(index.count());
+    while(attempts>1&&el>index.firstKey()&&index.count()<PREEXPAND){
+      index.expand(el);
+      attempts-=1;
+    }
+    for(var i=0;i<attempts;i++){
+      if(i>0) index.expand(el);
+      var encounter=select(el,index);
+      if(encounter!=null) return encounter;
+      if(el<index.firstKey()) break;
+    }
+    return null;
+  }
 
-	/** {@link #generatebyindex(int, List)} with {@link Terrain}s instead. */
-	public static Combatants generate(int el,List<Terrain> terrains){
-		var encounters=new ArrayList<EncounterIndex>(terrains.size());
-		for(var t:terrains)
-			encounters.add(Organization.ENCOUNTERSBYTERRAIN.get(t.toString()));
-		while(encounters.remove(null))
-			continue;
-		return generatebyindex(el,encounters);
-	}
+  /** {@link #generatebyindex(int, List)} with {@link Terrain}s instead. */
+  public static Combatants generate(int el,List<Terrain> terrains){
+    var encounters=new ArrayList<EncounterIndex>(terrains.size());
+    for(var t:terrains)
+      encounters.add(Organization.ENCOUNTERSBYTERRAIN.get(t.toString()));
+    while(encounters.remove(null)) continue;
+    return generatebyindex(el,encounters);
+  }
 
-	static Combatants select(int elp,EncounterIndex encounters){
-		var groups=encounters.get(elp);
-		return groups==null||groups.isEmpty()?null:RPG.pick(groups).generate();
+  static Combatants select(int elp,EncounterIndex encounters){
+    var groups=encounters.get(elp);
+    return groups==null||groups.isEmpty()?null:RPG.pick(groups).generate();
+  }
 
-		//		var els=new LinkedList<Integer>();
-		//		els.add(elp);
-		//		while(RPG.chancein(2)){
-		//			var el=els.remove(RPG.r(0,els.size()-1));
-		//			els.add(el-2);
-		//			els.add(el-2);
-		//		}
-		//		var foes=new Combatants();
-		//		for(var el:els){
-		//			var groups=encounters.get(el);
-		//			if(groups.isEmpty()) return null;
-		//			var group=RPG.pick(groups).generate();
-		//			foes.addAll(group);
-		//		}
-		//		if(!new AlignmentDetector(foes).check()) return null;
-		//		//		return foes.size()>getmaxenemynumber()?null:foes;]
-		//		return foes;
-	}
+  /** As {@link #generate(int, Terrain)} but for one terrain. */
+  public static Combatants generate(int el,Terrain t){
+    return generate(el,List.of(t));
+  }
 
-	//	private static void clean(EncounterIndex index){
-	//		if(Dungeon.active!=null||Period.NIGHT.is()||Period.EVENING.is()) return;
-	//		for(var el:index.keySet()){
-	//			var tier=index.get(el);
-	//			for(var g:new ArrayList<>(tier))
-	//				if(g.group.stream().filter(c->c.source.nightonly).findAny().isPresent())
-	//					tier.remove(g);
-	//		}
-	//	}
-
-	/** As {@link #generate(int, Terrain)} but for one terrain. */
-	public static Combatants generate(int el,Terrain t){
-		return generate(el,List.of(t));
-	}
-
-	/**
-	 * Generates an encounter given an EL. Even with {@link Haunt#getminimumel()},
-	 * it still may be hard to generate a particular EL from a pool, so we
-	 * iteratively look for the next-best thing before giving up, favoring lower
-	 * ELs before higher ELs.
-	 */
-	public static Combatants generate(int el,EncounterIndex index) throws GaveUp{
-		if(index.isEmpty()) throw new GaveUp();
-		var indexes=List.of(index);
-		var foes=generatebyindex(el,indexes);
-		if(foes!=null) return foes;
-		for(var delta=1;delta<=20;delta++){
-			foes=generatebyindex(el-delta,indexes);
-			if(foes!=null) return foes;
-			foes=generatebyindex(el+delta,indexes);
-			if(foes!=null) return foes;
-		}
-		throw new GaveUp();
-	}
+  /**
+   * Generates an encounter given an EL. Even with {@link Haunt#getminimumel()},
+   * it still may be hard to generate a particular EL from a pool, so we
+   * iteratively look for the next-best thing before giving up, favoring lower
+   * ELs before higher ELs.
+   */
+  public static Combatants generate(int el,EncounterIndex index) throws GaveUp{
+    if(index.isEmpty()) throw new GaveUp();
+    var indexes=List.of(index);
+    var foes=generatebyindex(el,indexes);
+    if(foes!=null) return foes;
+    for(var delta=1;delta<=20;delta++){
+      foes=generatebyindex(el-delta,indexes);
+      if(foes!=null) return foes;
+      foes=generatebyindex(el+delta,indexes);
+      if(foes!=null) return foes;
+    }
+    throw new GaveUp();
+  }
 }
